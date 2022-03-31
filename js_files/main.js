67e54c9bb6260b7561c965b2e2fb3c4d6b6b4481
EVGENIY <parukevich@gmail.com>
2022-03-31 23:02:52 +0300

    add upscroll

---
+++ js_files/main.js
@@ -0,0 +1,2 @@
+/* import {isCheckboxOrRadio, form, retrieveFormValue } from './submitForm';
+form.addEventListener('submit', retrieveFormValue) */
---
+++ js_files/scrollUp.js
@@ -0,0 +1,10 @@
+const upScroll = document.getElementById('upScroll');
+
+upScroll.onclick = function() {
+    window.scrollTo(pageXOffset, 0);
+    // после scrollTo возникнет событие "scroll", так что стрелка автоматически скроется
+};
+
+window.addEventListener('scroll', function() {
+    upScroll.hidden = (pageYOffset < document.documentElement.clientHeight);
+});
