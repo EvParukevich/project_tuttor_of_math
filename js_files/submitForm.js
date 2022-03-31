7dc449f9f67917bfb99db2b639175ecd700c1af8
EVGENIY <parukevich@gmail.com>
2022-03-31 22:14:26 +0300

    fix submit

--- js_files/submitForm.js
+++ js_files/submitForm.js
@@ -1,4 +1,26 @@
-const {form} = document.forms;
+/* const form = document.getElementById('contacts_form');
+
+console.log(form);
+function retrieveFormValue(event) {
+    event.preventDefault();
+
+   const name = form.querySelector('[name="name"]'),
+         telephone = form.querySelector('[name="telephone"]');
+
+    const values = {
+        name: name.value,
+        telephone: telephone.value
+    }
+
+    console.log('subObject', values);
+}
+
+form.addEventListener('submit', retrieveFormValue) */
+
+const isCheckboxOrRadio = type => ['checkbox', 'radio'].includes(type);
+
+// const {form} = document.forms;
+const form = document.getElementById('contacts_form');
 
 function retrieveFormValue(event) {
     event.preventDefault();
@@ -9,14 +31,14 @@ function retrieveFormValue(event) {
         const {name} = field;
 
         if (name) {
-            const {value} = field;
+            const {type, checked, value} = field;
 
-            values[name] = value;
+            values[name] = isCheckboxOrRadio(type) ? checked : value;
         }
     }
 
-    console.log(values);
+    // history.go();
+    console.log('submitObj', values);
 }
 
-
-form.addEventListener('submit', retrieveFormValue);
+form.addEventListener('submit', retrieveFormValue)
